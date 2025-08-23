361ccac9e174c3bab731da7a0f2984cb495ba280
Maksim <70702008m@gmail.com>
2025-08-23 16:55:57 +0300

    text

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+   const body = document.body;
+  const btnChangeColor = document.querySelector(".change-color");
+  const spanColor = document.querySelector(".color");
+   btnChangeColor.addEventListener("click", () => {
+    const newColor = getRandomHexColor();
+      body.style.backgroundColor = newColor;
+       spanColor.textContent = newColor;
+  });
