35f293accca15aff9e281013d3fa017cc5a8fe13
Maksim <70702008m@gmail.com>
2026-02-09 21:33:26 +0200

    text

--- js/task-77.js
+++ js/task-77.js
@@ -20,49 +20,67 @@
 // color.append(...nevcolor);
 
 
+    //                                                            2
+
 // const titleEl = document.querySelector(`.title`);
 // // titleEl.innerHTML = `<a href="/">Helloy</a>`;
 // // titleEl.innerHTML = ``;
 // titleEl.insertAdjacentHTML(`beforebegin`, ` <a href="/">Helloy</a>`);
+                                                                     
+   //                                                                 3
 
 
-const images = [
-  {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
-  },
-  {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  },
-  {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    alt: "Alpine Spring Meadows",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    alt: "Nature Landscape",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    alt: "Lighthouse Coast Sea",
-  },
-];
+// const images = [
+//   {
+//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+//     alt: "Alpine Spring Meadows",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+//     alt: "Nature Landscape",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+//     alt: "Lighthouse Coast Sea",
+//   },
+// ];
 
 
-const gallery = document.querySelector(".gallery");
-const gai = images
-    .map((item) =>
-    `<li class = "gallery-lalala">
-        <img src ="${item.url}" alt = "${item.alt}">
-</li>`
-).join("");
 
-gallery.insertAdjacentHTML(`beforebegin`, gai);
+
+// const gall = document.querySelector(`.gallery`);
+// const galer = images.map((item) =>
+//     `<li class = "gallery-lalala">
+// <img src = "${item.url}" alt = "${item.alt}">
+//     </li>`
+// ).join("");
+
+// gall.insertAdjacentHTML(`beforeend`, galer);
+
+
+  //                                                                4
+
+
+<input type="text" id="name-input" placeholder="Please enter your name" />
+<h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+
+
+
+
+
 
 
 
